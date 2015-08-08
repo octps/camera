17d65f8481462d2d327aa168776d8ec57409baed
octps <renmo9@gmail.com>
2015-08-08 14:19:56 +0900

    #1 レコード中にredordingと文字を表示

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
         onClickStopButton(sender as! UIButton)
     }
     
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stateLabel: UILabel!
+    
     let captureSession = AVCaptureSession()
     var previewLayer : AVCaptureVideoPreviewLayer?
     var captureDevice : AVCaptureDevice?
@@ -42,6 +46,7 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
                 }
             }
         }
+        stateLabel.text = ""
     }
     
     //カメラ設定
@@ -97,9 +102,8 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
             fileOutput.startRecordingToOutputFileURL(fileURL, recordingDelegate: self)
             
             isRecording = true
-            
-//            self.changeButtonColor(self.startButton, color: UIColor.grayColor())
-//            self.changeButtonColor(self.stopButton, color: UIColor.redColor())
+            stateLabel.text = "recording"
+            recordButton.setTitle("stop", forState: UIControlState.Normal)
         }
     }
     
@@ -108,8 +112,8 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
             fileOutput.stopRecording()
             
             isRecording = false
-//            self.changeButtonColor(self.startButton, color: UIColor.redColor())
-//            self.changeButtonColor(self.stopButton, color: UIColor.grayColor())
+            stateLabel.text = ""
+            recordButton.setTitle("start", forState: UIControlState.Normal)
         }
     }
     
@@ -121,77 +125,3 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
         assetsLib.writeVideoAtPathToSavedPhotosAlbum(outputFileURL, completionBlock: nil)
     }
 }
-
-//class ViewController: UIViewController {
-//    
-//    var stillImageOutput: AVCaptureStillImageOutput!
-//    var session: AVCaptureSession!
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//        
-//        // Start Camera
-//        self.configureCamera()
-//    }
-//    
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//    
-//    func configureCamera() -> Bool {
-//        // init camera device
-//        var captureDevice: AVCaptureDevice?
-//        let devices: NSArray = AVCaptureDevice.devices()
-//        
-//        // find back camera
-//        for device: AnyObject in devices {
-//            if device.position == AVCaptureDevicePosition.Back {
-//                captureDevice = device as? AVCaptureDevice
-//            }
-//        }
-//        
-//        if (captureDevice != nil) {
-//            // Debug
-//            print(captureDevice!.localizedName)
-//            print(captureDevice!.modelID)
-//        } else {
-//            print("Missing Camera")
-//            return false
-//        }
-//        
-//        
-//
-//       // let error:NSError?
-//        
-//        do {
-//            let deviceInput:AVCaptureDeviceInput = try AVCaptureDeviceInput(device: captureDevice) as AVCaptureDeviceInput
-//            self.stillImageOutput = AVCaptureStillImageOutput()
-//            
-//            // init session
-//            self.session = AVCaptureSession()
-//            self.session.sessionPreset = AVCaptureSessionPresetPhoto
-//            self.session.addInput(deviceInput as AVCaptureInput)
-//            self.session.addOutput(self.stillImageOutput)
-//
-//        } catch let error as NSError {
-//            print(error)
-//        }
-//        
-//        // init device input
-////        var error: NSErrorPointer!
-////        var deviceInput: AVCaptureInput = AVCaptureDeviceInput.deviceInputWithDevice(captureDevice, error: error) as AVCaptureInput
-//        
-//        
-//        // layer for preview
-//        let previewLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(self.session)
-//        previewLayer.frame = self.view.bounds
-//        self.view.layer.addSublayer(previewLayer)
-//        
-//        self.session.startRunning()
-//        
-//        return true
-//    }
-//    
-//}
